b26a9202a8f3f6c9105a4fcd0fe538bd33b02608
reiy3s <reyes.am@obmontessori.edu.ph>
2026-09-15 18:46:47 +0800

    fix?

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from pyscript import display, document
 
 def create_order(e):
-    document.getElementById("output2").innerHTML = "" # clears previous output
+    document.getElementById("receipt").innerHTML = "" # clears previous output
+
     prod1 = document.getElementById("item1")
+    price1 = float(prod1.value) * prod1.checked
+
     prod2 = document.getElementById("item2")
+    price2 = float(prod2.value) * prod2.checked
+
     prod3 = document.getElementById("item3")
+    price3 = float(prod3.value) * prod3.checked
+
     prod4 = document.getElementById("item4")
+    price4 = float(prod4.value) * prod4.checked
 
     # Calculate
-    subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked
-    size = document.querySelector('input[name="size"]:checked')
-    size_price = float(size.value)
-    grandtotal = subtotal + size_price
+    subtotal = price1 + price2 + price3 + price4
+    tax = subtotal * 0.12
+    grandtotal = subtotal + tax
 
     #display(grandtotal, target="output1")
 
-def create_order(e):
-    document.getElementById("output2").innerHTML = "" # clears previous output
-    coffee = document.getElementById("coffee")
-    coffee_price = float(coffee.value)
-    display(f'Pay {coffee_price}', target = "output2")
+    display(f'⊹ Receipt ⊹', target = "receipt")
+    display(f'Your total is Php.{grandtotal}!', target = "receipt")
